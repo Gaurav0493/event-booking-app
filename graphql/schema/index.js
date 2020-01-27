64425498b0423f7a8f0a37555ea8531814a1fc09
Gaurav0493 <gauravkr93.gk@gmail.com>
2020-01-27 23:23:43 +0530

    Refactored the code for readability

--- graphql/schema/index.js
+++ graphql/schema/index.js
@@ -1,7 +1,6 @@
 const { buildSchema } = require('graphql');
 
 module.exports = buildSchema(`
-
 type Booking {
     _id: ID!
     event: Event!
@@ -9,49 +8,42 @@ type Booking {
     createdAt: String!
     updatedAt: String!
 }
-
-type Event { 
-    _id: ID!,
-    title: String!,
-    description: String!,
-    price: Float!,
-    date: String!,
-    creator: User!
+type Event {
+  _id: ID!
+  title: String!
+  description: String!
+  price: Float!
+  date: String!
+  creator: User!
 }
-
 type User {
-    _id: ID!,
-    email: String!,
-    password: String,
-    createdEvents: [Event!]
+  _id: ID!
+  email: String!
+  password: String
+  createdEvents: [Event!]
 }
-
 input EventInput {
-    title: String!,
-    description: String!,
-    price: Float!,
-    date: String!
+  title: String!
+  description: String!
+  price: Float!
+  date: String!
 }
-
 input UserInput {
-    email: String!,
-    password: String!
+  email: String!
+  password: String!
 }
-
 type RootQuery {
     events: [Event!]!
     bookings: [Booking!]!
 }
-
 type RootMutation {
     createEvent(eventInput: EventInput): Event
     createUser(userInput: UserInput): User
     bookEvent(eventId: ID!): Booking!
     cancelBooking(bookingId: ID!): Event!
 }
-
 schema {
     query: RootQuery
     mutation: RootMutation
 }
-`)
+`);
